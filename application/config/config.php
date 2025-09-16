717f0a03289325692011292d612b448e0fab0466
Jithin <jithin143jithu@gmail.com>
2025-09-16 19:51:57 +0530

    session time commeted Time zone set fro session

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Kolkata');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -388,10 +389,10 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
-$config['sess_expiration'] = 7200;
+// $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+// $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
