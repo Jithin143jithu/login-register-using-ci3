eee46cba180f8449b07aa5f55d2743f25afd3b06
Jithin <jithin143jithu@gmail.com>
2025-09-17 14:20:24 +0530

    view user script added

---
+++ assets/js/viewUser.js
@@ -0,0 +1,25 @@
+/**
+ * File : viewUser.js
+ * 
+ * This file contain the validation of add user form
+ * 
+ * Using validation plugin : jquery.validate.js
+ * 
+ * @author Jithin VP
+ */
+
+$(document).ready(function(){
+    $(function () {
+    $('#example1').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : true,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+
+});
+
+
