647d600fbf7d3f8a176f8d4fbcb1993f4e123dac
Jithin <jithin143jithu@gmail.com>
2025-09-17 15:10:59 +0530

    db staic changed to dynamic

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,10 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+// application/config/constants.php
+defined('DB_PERMISSIONS')  OR define('DB_PERMISSIONS', 'permissions');
+defined('DB_ROLES')  OR define('DB_ROLES', 'roles');
+defined('DB_ROLE_PERMISSIONS') OR define('DB_ROLE_PERMISSIONS', 'role_permissions');
+defined('DB_USERS') OR define('DB_USERS', 'users');
